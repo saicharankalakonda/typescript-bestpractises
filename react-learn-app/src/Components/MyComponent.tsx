608603db57c490a04723d81cc439cded5ea73c0a
saicharankalakonda <ksaicharan29@gmail.com>
2024-05-01 12:14:19 +0530

    removing without mutating original object

--- react-learn-app/src/Components/MyComponent.tsx
+++ react-learn-app/src/Components/MyComponent.tsx
@@ -1,8 +1,15 @@
 import RenderIf from "./RenderIf";
 
 
+export const removeKey = (key:any,obj:any) =>{
+    const{[key]:omitted,...res} = obj;
+    return res;
+}
 
- const MyComponent = ({dataIsReady,isAdmin,userHasPermissions}) =>{
+ const MyComponent = ({dataIsReady,isAdmin,userHasPermissions}:any) =>{
+
+    console.log(removeKey("bar",{foo:1,bar:2}));
+
     return (
         <div>
             <RenderIf isTrue={dataIsReady && (isAdmin || userHasPermissions)}>
